6fb8aeaba2b58d5e7de904a1e40db23695ec4f41
Bimbok <bimbokmkj@gmail.com>
2026-04-05 08:54:04 +0530

    feat: theme-fix

--- plugins/tmux-nerd-font-window-name/shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  buildInputs = with pkgs;[
-    tmux
-    nodePackages.bash-language-server
-    # bashunit: install via 'curl -s https://bashunit.typeddevs.com/install.sh | bash'
-  ];
-}
